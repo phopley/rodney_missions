7796c1aaa89c0271b323f213ac5e99bb04fd6675
phopley <philip@thehopleys.me.uk>
2019-04-09 16:13:55 +0100

    added copyright notice

--- src/missions_lib/greet_all.py
+++ src/missions_lib/greet_all.py
@@ -1,7 +1,20 @@
 #!/usr/bin/env python
+# Copyright 2019 Philip Hopley
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not 
+# use this file except in compliance with the License. You may obtain a  copy
+# of the License at
+#
+#  http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software 
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations under
+# the License.
+#
 # States for search for people by moving the head only and to greet people
 # that it sees who are known
-
 import rospy
 from smach import State, StateMachine
 from smach_ros import SimpleActionState
